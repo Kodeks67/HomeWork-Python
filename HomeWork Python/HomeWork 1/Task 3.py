5d275376e14b16f770a18a38300160699ba25ad8
Zaur <zaur.cherchesov@mail.ru>
2023-06-15 16:53:23 +0300

    added second method Task 3

--- HomeWork Python/HomeWork 1/Task 3.py
+++ HomeWork Python/HomeWork 1/Task 3.py
@@ -1,9 +1,25 @@
+# Способ решения задачи №1
 def LuckyTicket(number):
-    if (number // 100000) % 10 + (number // 10000) % 10 + (number // 1000) % 10 == (number // 100) % 10 + (number % 100) // 10 + number % 10:
-        print("Билетик Счастливый УРА!")
+    if (number // 100000) % 10 + (number // 10000) % 10 + (number // 1000) % 10 == (number // 100) % 10 + \
+            (number % 100) // 10 + number % 10:
+        print("Счастливый билет УРА!!!")
     else:
-        print("Билетик не счастливый")
+        print("Не счастливый билетик(")
 
 
-number = int(input())
+number = int(input('Введите шестизначное число: '))
 LuckyTicket(number)
+
+
+# Способ решения задачи №2
+
+def LuckyTicketSTR(number):
+    num = str(number)
+    num1, num2, num3, num4, num5, num6 = int(num[0]), int(num[1]), int(num[2]), int(num[3]), int(num[4]), int(num[5])
+    if num1 + num2 + num3 == num4 + num5 + num6:
+        print('Счастливый билет УРА!!!')
+    else:
+        print('Не счастливый билетик(')
+
+
+LuckyTicketSTR(number)
